2cf6a58d0969cef9bbf641cda617896e11330c94
Jeremy Chin <rekless@fastmail.fm>
2012-09-06 22:13:43 +1000

    QC Filtering, parallel processing.

--- extract_hands.py
+++ extract_hands.py
@@ -5,6 +5,9 @@ import tenhou
 import sys
 import json
 import time
+import os.path
+import os
+import multiprocessing
 
 # STORAGE FORMAT
 # hands: [[tile]*1-13]*4 (tile 1-135)
@@ -19,61 +22,91 @@ import time
 # round: current round number (0-12, 0-3 is east, 4-7 is south...)
 # dora: list of dora (indicators? not yet confirmed)
 
-id = int(time.time() * 1000)
+def crunch_game(file):
+    game = tenhou.run_log(os.path.join(sys.argv[1], file))
+    # 2009022011gm-00e1-0000-42d20b88
+    idx = 0
+    print file
 
-game = tenhou.run_log(sys.stdin)
-for state in game:
-    if state.event == 'agari':
-        # state.data is the event data (i.e. the deal-in), state.dealer is the
-        # east player
-        if state.data['dealer'] == state.player:
-            continue # tsumo, don't record
+    for state in game:
+        if state.event == 'agari':
+            idx += 1
+            # state.data is the event data (i.e. the deal-in), state.dealer is the
+            # east player
+            if state.data['dealer'] == state.player:
+                continue # tsumo, don't record
+            # Hand quality control filters
+            player = state.players[state.player]
+            # too few discards is unreadable
+            if len(player.discards) < 6:
+                continue
+            # dama hits are boring
+            if player.riichitile is None and len(player.melds) == 0:
+                continue
+            # Too easy to read if dealer is tenpai
+            if tenhou.agari_tiles([x/4 for x in
+                state.players[state.data['dealer']].hand]):
+                continue
 
-        # Players need to be rearranged so that 0 is the one who dealt
-        # in
-        player_order = [(x + state.data['dealer']) % 4 for x in range(4)]
-        order_inverse = [(x - state.data['dealer']) % 4 for x in range(4)]
-        winner = order_inverse[state.player]
-        hands = []
-        discards = []
-        melds = []
-        riichi = []
+            # Players need to be rearranged so that 0 is the one who dealt
+            # in
+            player_order = [(x + state.data['dealer']) % 4 for x in range(4)]
+            order_inverse = [(x - state.data['dealer']) % 4 for x in range(4)]
+            winner = order_inverse[state.player]
+            hands = []
+            discards = []
+            melds = []
+            riichi = []
 
-        for p in player_order:
-            player = state.players[p]
-            hands.append(sorted(list(player.hand)))
-            # Filter the discards. Pack the flags into the tile number
-            discards.append([tile + (flag * 1024) for tile, flag in player.discards])
-            p_melds = []
-            for meld in player.melds:
-                p_melds.append({'tiles': meld['tiles'],
-                    'type': meld['type'],
-                    'dealer': order_inverse[meld['dealer']]})
-            melds.append(p_melds) #need to remap some numbers in here
+            for p in player_order:
+                player = state.players[p]
+                hands.append(sorted(list(player.hand)))
+                # Filter the discards. Pack the flags into the tile number
+                discards.append([tile + (flag * 1024) for tile, flag in player.discards])
+                p_melds = []
+                for meld in player.melds:
+                    p_melds.append({'tiles': meld['tiles'],
+                        'type': meld['type'],
+                        'dealer': order_inverse[meld['dealer']]})
+                melds.append(p_melds) #need to remap some numbers in here
 
-            if player.riichitile in player.discards:
-                riichi.append(player.discards.index(player.riichitile))
-            else:
-                riichi.append(-1)
+                if player.riichitile in player.discards:
+                    riichi.append(player.discards.index(player.riichitile))
+                else:
+                    riichi.append(-1)
 
-        # This state is captured just after the player dealt in - load the tile
-        # back into the hand
-        player = order_inverse[state.data['dealer']]
-        tile = discards[player].pop() % 1024
-        hands[player].append(tile)
-        hands[player].sort() # Don't let the hot tile sit at the end!
+            # This state is captured just after the player dealt in - load the tile
+            # back into the hand
+            player = order_inverse[state.data['dealer']]
+            tile = discards[player].pop() % 1024
+            hands[player].append(tile)
+            hands[player].sort() # Don't let the hot tile sit at the end!
 
-        waits = tenhou.agari_tiles([x/4 for x in hands[winner]])
+            waits = tenhou.agari_tiles([x/4 for x in hands[winner]])
 
-        f = open("hand%08d" % id, "w")
-        json.dump({'hands': hands,
-            'discards': discards,
-            'melds': melds,
-            'riichi': riichi,
-            'waits': waits,
-            'player': order_inverse[state.player],
-            'dealer': order_inverse[state.dealer],
-            'round': state.round, 
-            'dora': state.dora}, f)
-        f.close()
-        id += 1
+            f = open("%s-%03d" % (file, idx), "w")
+            json.dump({'hands': hands,
+                'discards': discards,
+                'melds': melds,
+                'riichi': riichi,
+                'waits': waits,
+                'player': order_inverse[state.player],
+                'dealer': order_inverse[state.dealer],
+                'round': state.round, 
+                'dora': state.dora}, f)
+            f.close()
+def crunch_nofail(file):
+    try:
+        crunch_game(file)
+    except Exception, e:
+        print e
+
+
+if __name__ == '__main__':
+
+    files = os.listdir(sys.argv[1])
+    print "Processing %d files" % len(files)
+    pool = multiprocessing.Pool(4)
+    pool.map(crunch_nofail, files)
+
+
