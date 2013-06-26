4eacb1d4d2e584513aa0156b273354723de70d2c
Jeremy Chin <jeremyc@f-m.fm>
2013-06-26 22:48:57 +0900

    Added score drilling page.

--- score_table.py
+++ score_table.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 
 import math
 
@@ -16,38 +16,50 @@ def score(fu, han, dealer = False):
 		oya = math.ceil(base*2) * 100
 	ko = math.ceil(base) * 100
 
-	return (tsumo, ko, oya)
+	return (tsumo, ko, oya, "%s_%s" % (han, "o" if dealer else "k"))
 
 table = [[4, 3, 2, 1, '', 1, 2, 3, 4]]
 for fu in xrange(20, 80, 10):
 	row = [score(fu, x, True) for x in range(4, 0, -1)]
 	row.append(fu)
 	row.extend([score(fu, x, False) for x in range(1, 5)])
-	table.append(row)
+	table.append((fu, row))
 
 def display(col):
+
+	if isinstance(col, int):
+		return "<td><h2>%s</h2></td>" % col
 	# dealer ron-only corner
 	# ko only-only corner
 	# Mangan cases
 	# Everything else
-	pass
+	
+	if col[2] != -1:
+		rest = "%d/%d" % (col[2], col[1])
+	else:
+		rest = "%d" % col[1]
+
+	return '<td class="%s_han"><b>%d</b><br />%s</td>' % (col[3], col[0], rest)
 
 # Header row
 print "<table>\n<tr>"
 for col in table[0]:
-	print "<th>%s</th>" % col,
+	print "<th>%s</th>" % col
 print "</tr>\n<tr>"
 
 # 20fu row - no tsumo
-for col in table[1]:
-	print "<td>%s</td>" % col
-print "</tr>" # Opened at the close of the thead above
+#for col in table[1]:
+#	print "<td>%s</td>" % str(col)
+#print "</tr>" # Opened at the close of the thead above
 
 for row in table[1:]:
-	print "<tr>"
-	for col in row:
-		print "<td>",
-		print col,
-		print "</td>"
+	print '<tr class="%s_fu">' % row[0]
+	for col in row[1]:
+		#print "<td>",
+		# print col
+		print display(col)
+		#print col,
+		#print "</td>"
 	print "</tr>"
 
+print "</table>"
